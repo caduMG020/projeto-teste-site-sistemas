62aebdcf6fda60188ae6e0efe6b0ca48ea221069
caduMG020 <cadumg566@gmail.com>
2026-02-06 23:25:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,21 @@ function adicionarAoCarrinho(nome, preco) {
   const item = document.createElement("li");
   item.textContent = `${nome} - R$ ${preco.toFixed(2)}`;
 
+  const botaoRemover = document.createElement("button");
+  botaoRemover.textContent = "X";
+  botaoRemover.style.marginLeft = "10px";
+
+  botaoRemover.onclick = function () {
+    lista.removerChild(item);
+    total -= preco;
+    
+  document.getElementById("total").textContent = total.toFixed(2);
+  };
+  
+  item.appendChild(botaoRemover);
   lista.appendChild(item);
 
   total += preco;
+  
   document.getElementById("total").textContent = total.toFixed(2);
 }
